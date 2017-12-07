96c9006411fbab9161cbb9f20729bf6ba8624ea1
xiiaoyumi <yuuki_asunaaaa@hotmail.com>
2017-12-07 12:04:22 +0800

    initial

---
+++ app/src/main/java/my/edu/tarc/lab33navidrawer/ImportFragment.java
@@ -0,0 +1,45 @@
+package my.edu.tarc.lab33navidrawer;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ImportFragment extends Fragment {
+
+    private TextView textViewMessage;
+
+
+    public ImportFragment() {
+        // Required empty public constructor
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_import, container, false);
+        textViewMessage = (TextView) v.findViewById(R.id.textViewMessage);
+        final Button buttonShowMessage = (Button) v.findViewById(R.id.buttonShowMessage);
+        buttonShowMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewMessage.setText("Say cheese");
+            }
+        });
+
+        // Inflate the layout for this fragment
+        return v;
+    }
+
+}
